0dc7439e07a1a25119026019b013ed7f1e196c68
eltorky1983-debug <eltorky1983@gmail.com>
2026-04-29 07:40:26 +0300

    System: Connection optimization and fresh build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-// Force rebuild trigger: 2026-04-28-2319
+// Force rebuild trigger: 2026-04-29-vFinal
 const nextConfig = {
   images: {
     remotePatterns: [
